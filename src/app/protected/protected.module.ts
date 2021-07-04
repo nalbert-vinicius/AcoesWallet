50ca9fff13dd2d8af86f60dbae04fbda99ce69f6
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-07-04 11:01:30 -0300

    Adicionando angular material

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ProtectedRoutingModule } from './protected-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -11,7 +12,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
-    ProtectedRoutingModule
+    ProtectedRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class ProtectedModule { }
