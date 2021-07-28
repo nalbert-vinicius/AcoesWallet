cbb825c12e93067b9618b8599fb10d9fa1562149
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-07-27 21:12:24 -0300

    deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/authApp'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/authApp/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log("SERVIDOR NA PORTA "+ PORT);
+})
+
